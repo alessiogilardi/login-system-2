290d21a9f6ff2e3f6ab72747b1465e7b03614532
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-05-29 10:47:41 +0200

    deleted

--- API/redirect.php
+++
@@ -1,4 +0,0 @@
-<?php
-if($_SERVER['PHP_SELF']=='/base_function.php') header('Location: index.php');
-// if(basename($_SERVER['PHP_SELF']) == basename($_SERVER['SCRIPT_FILENAME'])) header('Location: index.php');
-?>
