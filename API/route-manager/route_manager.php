de7e6b4c95e1f74f8d353fffef8d0cd05fb05523
alessiogilardi <36900296+alessiogilardi@users.noreply.github.com>
2018-06-10 22:41:12 +0200

    edit

--- API/route-manager/route_manager.php
+++ API/route-manager/route_manager.php
@@ -7,7 +7,8 @@ class RouteManager {
 	public static function getCurrentUri() {
         $basepath = implode('/', array_slice(explode('/', $_SERVER['SCRIPT_NAME']), 0, -1)) . '/';
         $uri = substr($_SERVER['REQUEST_URI'], strlen($basepath));
-        if (strstr($uri, '?')) $uri = substr($uri, 0, strpos($uri, '?'));
+        if (strstr($uri, '?'))
+        	$uri = substr($uri, 0, strpos($uri, '?'));
         $uri = '/' . trim($uri, '/');
         return $uri;
     }
